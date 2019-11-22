e4454dd9d6d863df282784fd53eeebee6ee34663
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-22 00:28:17 -0700

    Create seeds file for users

---
+++ db/seeds/dev/users.js
@@ -0,0 +1,13 @@
+exports.seed = function (knex) {
+  return knex('users').del()
+    .then(() => {
+      return Promise.all([
+        knex('users').insert({
+          api_key: 'leiyak1234'
+        }, 'id')
+          .then(() => console.log('Seeding complete!'))
+          .catch(error => console.log(`Error seeding data: ${error}`))
+      ])
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`))
+}
