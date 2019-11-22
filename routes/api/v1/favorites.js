da21b5f85d3b4c3ba29cb1e5c9042d1a8d20d659
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-22 05:37:23 -0700

    Update to add post for adding favourite location, helpers, and forecast obj

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var forecastObject = require('../../../models/forecast.js');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
+const helpers = require('../../../helpers/helpers');
+const formattedForecast = helpers.formatForecast;
 
 router.get('/', (request, response) => {
   const userApiKey = request.body.api_key;
@@ -18,11 +21,38 @@ router.get('/', (request, response) => {
         .catch(error => {response.status(500).json({ error });
       });
     } else {
-      return response.status(401).json({ error: "Unauthorized: missing or invalid API key" });
+      return response.status(401).json({ error: "Unauthorized: invalid or missing API key" });
     }
   });
 });
 
+
+router.post('/', (request, response) => {
+  (async () => {
+   const favorite = request.body
+   const userId = await helpers.userByKey(favorite.api_key).then(function (result) { return result })
+
+   if (userId) {
+     for (const requiredParameter of ['location', 'api_key']) {
+       if (!favorite[requiredParameter]) {
+         return response
+           .status(422)
+           .send({ error: `Expected format: { location: <STRING>, api_key: <STRING}. You're missing a "${requiredParameter}" property.` })
+       }
+     }
+
+     database('favorites').insert({ location: await favorite.location, user_id: await userId.id })
+       .then(like => {
+         response.status(200).json({ message: `${favorite.location} has been added to your favorites` })
+       })
+       .catch(error => {
+         response.status(500).json({ error })
+       })
+   } else {
+     response.status(401).send({ error: 'Unauthorized: incorrect or missing API key' })
+   }
+ })()
+});
 // router.post('/', (request, response) => {
 //   (async() => {
 //     const favorite = request.body
