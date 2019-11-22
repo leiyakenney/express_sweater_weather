e6e2274d7dfe13f8bb4ab432ae00f1e3679a7c44
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-21 22:54:48 -0700

    Break apiCoordinates into two functions and adds missing awaits

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -6,13 +6,21 @@ const fetch = require("node-fetch");
 async function apiCoordinates(location) {
   let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_API_KEY}`);
   let data = await response.json();
-  return data.results[0].geometry.location;
+  return data;
 }
 
-async function apiForecast(coordinates) {
-  let lat = coordinates.lat
-  let long = coordinates.lng
-  let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${lat},${long}?exclude=minutely,alerts,flags`);
+async function coordinates(location) {
+  let data = await apiCoordinates(location);
+  console.log(data)
+  let coords = data.results[0].geometry.location;
+  return coords;
+}
+
+async function apiForecast(location) {
+  let coords = await coordinates(location);
+  // let lat = await coordinates.lat;
+  // let long = await coordinates.lng;
+  let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${coords.lat},${coords.lng}`);
   let forecast = await response.json();
   return forecast;
 }
