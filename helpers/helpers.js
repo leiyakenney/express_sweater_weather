46080121721a6a4a5eef4c8c31bad35fca75727c
Leiya Kenney <leiya.kenney@gmail.com>
2019-11-21 23:35:38 -0700

    Add forecast POJO and formatForecast functions

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,6 +2,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 const fetch = require("node-fetch");
+const Forecast = require("../models/forecast");
 
 async function apiCoordinates(location) {
   let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_API_KEY}`);
@@ -18,14 +19,19 @@ async function coordinates(location) {
 
 async function apiForecast(location) {
   let coords = await coordinates(location);
-  // let lat = await coordinates.lat;
-  // let long = await coordinates.lng;
   let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${coords.lat},${coords.lng}?exclude=minutely,alerts,flags`);
   let forecast = await response.json();
   return forecast;
 }
 
+async function formatForecast(location) {
+  let forecast = await apiForecast(location);
+  let formattedForecast = await new Forecast(location, forecast);
+  return formattedForecast;
+}
+
 module.exports = {
   apiCoordinates: apiCoordinates,
-  apiForecast: apiForecast
+  apiForecast: apiForecast,
+  formatForecast: formatForecast
 }
